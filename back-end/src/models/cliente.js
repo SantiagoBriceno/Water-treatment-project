7fb2644c6b068f97a20854a7bf8cdfb1cbf59c70
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2024-07-16 13:50:18 -0400

    Added: Created Cliente Mongoose Model

---
+++ back-end/src/models/cliente.js
@@ -0,0 +1,34 @@
+/*
+id (autoincremental)
+rif/cedula
+Nombre y apellido
+tlf
+direccion (opcional)
+*/
+
+import { Schema, model } from 'mongoose'
+
+const clienteSchema = new Schema({
+  documento: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  nombre: {
+    type: String,
+    required: true
+  },
+  telefono: {
+    type: String,
+    required: true
+  },
+  direccion: {
+    type: String
+  }
+},
+{
+  timestamps: true,
+  versionKey: false
+})
+
+export const Cliente = model('Cliente', clienteSchema)
