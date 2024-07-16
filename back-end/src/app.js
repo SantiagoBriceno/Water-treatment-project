cb8a068ce0bbcab66aaf2e3b18ad3710d687df4a
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2024-07-16 13:50:28 -0400

    Added: Clientes routes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,13 @@
 import { pool } from './db.js'
 import express from 'express'
 import cors from 'cors'
+import clientesRouter from './routes/v1/clientes.routes.js'
 
 pool()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('api/v1/clientes', clientesRouter)
 
 export default app
