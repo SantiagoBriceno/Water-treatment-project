6153a1162ab27edc6a65778f54c748776e519934
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2024-07-22 13:28:47 -0400

    To Fixed: Add logo

--- front-end/src/components/partials/Header-sidebar.jsx
+++ front-end/src/components/partials/Header-sidebar.jsx
@@ -1,11 +1,9 @@
 import {
   IconButton,
-  Avatar,
   Box,
   CloseButton,
   Flex,
   HStack,
-  VStack,
   Icon,
   useColorModeValue,
   Text,
@@ -52,12 +50,12 @@ const SidebarContent = ({ onClose, ...rest }) => {
     >
       <Flex h='20' alignItems='center' mx='8' justifyContent='space-between'>
         <Text fontSize='2xl' fontFamily='monospace' fontWeight='bold'>
-          Logo
+          Purificadora
         </Text>
         <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
       {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
+        <NavItem key={link.name} icon={link.icon} to={link.to}>
           {link.name}
         </NavItem>
       ))}
@@ -140,7 +138,8 @@ const MobileNav = ({ onOpen, ...rest }) => {
         <Flex alignItems='center'>
           <Menu>
             <MenuButton py={2} transition='all 0.3s' _focus={{ boxShadow: 'none' }}>
-              <HStack>
+              {/* Si queremos avatar de usuario */}
+              {/* <HStack>
                 <Avatar
                   size='sm'
                   src='https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
@@ -159,7 +158,7 @@ const MobileNav = ({ onOpen, ...rest }) => {
                 <Box display={{ base: 'none', md: 'flex' }}>
                   <FiChevronDown />
                 </Box>
-              </HStack>
+              </HStack> */}
             </MenuButton>
             <MenuList
               bg={useColorModeValue('white', 'gray.900')}
